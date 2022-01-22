615ed2913e43a0a1ca183e96b26518d3c38fa778
Tina-Bambina <speleerska@gmail.com>
2022-01-22 15:19:15 +0100

    1 file modified

--- index.js
+++ index.js
@@ -1,15 +1,9 @@
-/*
-
-import './ukol1.js'
-import './ukol2.js'
-import './ukolprvni.js';
-import './ukoldruhy.js';
 
 
+//import './ukol1.js'
+//import './ukol2.js'
+//import './ukolprvni.js';
+//import './ukoldruhy.js';
+//import './ukoltreti.js';
 
-*/
-
-
-import './ukoltreti.js';
-//
-//import './ukolctvrty.js';
+import './ukolctvrty.js';
