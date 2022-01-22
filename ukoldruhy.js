9dfecad832cb541bf937b3838889df6642f56215
Tina-Bambina <speleerska@gmail.com>
2022-01-22 13:27:49 +0100

    1 file modified

--- ukoldruhy.js
+++ ukoldruhy.js
@@ -5,6 +5,7 @@
  b) Vaše jméno je Marian, je vám 14 let a tak musíte počkat ještě 4 let, než budete moci řídit auto.
 */
 
+/*
 let jmeno = prompt('Zadej sve jmeno:');
 let vek = Number(prompt('Zadej svuj vek'));
 
@@ -17,10 +18,47 @@ console.log('Vase jmeno je ' + jmeno + ' je vam ' + vek + ' a tak mate pravo rid
   console.log('Vase jmeno je ' + jmeno + ' je vam ' + vek + ' a tak musíte počkat ještě '+ rozdil + ' let, než budete moci řídit auto');
 }
 
-
+*/
 
 
 /*
 
 2)  Nechte uživatele zadat 3 čísla a určete největší z nich - výsledek vypište do konzole ve formátu "Největší číslo je A: 37".​
 } */
+
+
+
+/*
+let A = Number( prompt('Zadejte cislo A:'));
+let B = Number( prompt('Zadejte cislo B:'));;
+let C = Number( prompt('Zadejte cislo C:'));
+
+console.log("Nejvyssi z Vami vlozenych cisel je:" + Math.max(A,B,C));
+
+*/
+
+
+
+let A = Number( prompt('Zadejte cislo A:'));
+let B = Number( prompt('Zadejte cislo B:'));;
+let C = Number( prompt('Zadejte cislo C:'));
+
+if (A>B && A>C)
+{
+  console.log("Nejvyssi z Vami vlozenych cisel je cislo A: " + A)
+}else if (B>A && B>C)
+{
+  console.log("Nejvyssi z Vami vlozenych cisel je cislo B: " + B)
+}
+else 
+console.log("Nejvyssi z Vami vlozenych cisel je cislo C: " + C)
+
+
+
+
+
+
+
+
+
+
