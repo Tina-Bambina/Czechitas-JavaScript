1b7a55eb5641fb84761f87e59b3c3c51c17799ca
Tina-Bambina <speleerska@gmail.com>
2022-01-22 14:14:06 +0100

    1 file modified

--- ukoltreti.js
+++ ukoltreti.js
@@ -17,4 +17,20 @@ while (num!=0)
   console.log(num+=num);}
 
 
-  console.log()
+  console.log('pozrrav4');
+
+
+
+
+/*
+  2) Nechte uzivatele zadat 2 cisla, ktere znaci dolni a horni mez na ciselne ose celych cisel - vcetne, pokud tedy zada 3 a 7, uvazujme cisla 3, 4, 5, 6, 7. Vypište pomocí FOR cyklu pouze sudá čísla.
+*/
+
+
+/*
+  3) Prepiste tento for cyklus pomoci While cyklu
+    for (let i = 12, j = 12 ** 3; i < 12 + j; i = i * 2) {
+      console.log(i);
+    }
+  - poznamka - pripomenout definici casti for cyklu
+*/
